afb9b547ca991f4ea9c5a0be8c4c93efbf725e23
MOHAMED Hamda <33846477+genidevelop@users.noreply.github.com>
2024-04-22 10:18:05 +0100

    Manipulating arrays

---
+++ Week02/scores/scores.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+
+const int N = 3;
+
+// Prototype
+float average(int length, int array[]);
+
+int main(void)
+{
+    // Get scores
+    int scores[N];
+    for (int i = 0; i < N; i++)
+    {
+        scores[i] = get_int("Score: ");
+    }
+
+    printf("Average: %f\n", average(N, scores));
+}
+
+// Calculates the average
+float average(int length, int array[])
+{
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        sum += array[i];
+    }
+    return sum / (float) length;
+}
